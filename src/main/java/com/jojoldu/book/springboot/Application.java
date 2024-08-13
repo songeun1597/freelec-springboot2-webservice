526a75adf54809862be473e479672b0d744767a0
songeun1597 <163233402+songeun1597@users.noreply.github.com>
2024-08-13 12:13:03 +0900

    Hello Controller

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book;
+package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,5 +8,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
